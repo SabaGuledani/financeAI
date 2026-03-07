16a39cc76fff9f8181ada9d873805b8f8b86de00
saba_guledani <115211537+SabaGuledani@users.noreply.github.com>
2026-03-07 18:14:11 +0400

    move excel parsing to services

---
+++ backend/app/services/extract_payments.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import re
+
+def get_payments_df(transactions_df:pd.DataFrame) -> pd.DataFrame:
+    """separates actual payments from just transactions"""
+    def find_transaction_object(text):
+        pattern = r"ობიექტი:\s*([^,]+)"
+        res = re.search(pattern, text)
+        if res:
+            return res.group(1)
+
+    payments_df = transactions_df.loc[transactions_df["transaction_type"] == "გადახდა"] # separate actal payments from transactions
+    payments_df["transaction_object"] = payments_df["დანიშნულება"].apply(lambda x:find_transaction_object(x) ) # separate where transaction was actually performed
+    payments_df["transaction_object"] = payments_df["transaction_object"].fillna(value="gadakhda") # fill wherever object was unavailable
+    payments_df.drop(columns=["transaction_type"],inplace=True)
+    payments_df.fillna(value=0, inplace=True) # fill other NaN values
+    return payments_df
