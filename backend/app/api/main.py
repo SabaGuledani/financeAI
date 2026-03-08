52e076e49af4839372721a6b5c9ee025180ed8db
saba_guledani <115211537+SabaGuledani@users.noreply.github.com>
2026-03-08 15:16:28 +0400

    startup

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -2,5 +2,9 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 from app.utils.init_db import create_db
+from fastapi import FastAPI
+from app.api.routes_upload import router
 
 create_db()
+
+app = FastAPI()
